703f4be5cbe6ea6ed2afe1bb26ee47295e04f584
SpaceCreeper <96059856+SpaceCreeper@users.noreply.github.com>
2024-06-20 00:37:27 +0800

    Create PyGames.py

---
+++ PyGames.py
@@ -0,0 +1,32 @@
+import random
+import time
+
+print("Welcome to PyGames!\nPick a game to play:")
+
+choice = input("[1] Scissors, Paper, Stone\n")
+
+if(choice == 1):
+    roundCount = 0
+    compChoice = 0
+    playerChoice = 0
+
+    print("Welcome to Scissors, Paper and Stone!")
+
+    print("Round " + roundCount)
+
+    compChoice = random.randint(1, 3)
+    playerChoice = input("Pick your choice: ")
+
+#Tie
+    if(compChoice == 1 and playerChoice == 1):
+        print("It's a tie!")
+    elif(compChoice == 2 and playerChoice == 2):
+        print("It's a tie!")
+    elif(compChoice == 3 and playerChoice == 3):
+        print("It's a tie!")
+
+#Computer Wins
+    if(compChoice == 1 and playerChoice == 2):
+        print("Player has won!")
+    elif(compChoice == "thing" and playerChoice == "thing"):
+        print("Player has won!")
